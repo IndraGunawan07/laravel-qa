953bec988b69678f2af2b2988df51ccd56ef33ce
IndraGunawan07 <53326638+IndraGunawan07@users.noreply.github.com>
2020-08-13 00:35:28 +0700

    Implement many to many polymorphic relationship and make the vote question controls working

--- app/Answer.php
+++ app/Answer.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Answer extends Model
 {
@@ -60,5 +61,10 @@ class Answer extends Model
     public function isBest()
     {
         return $this->id === $this->question->best_answer_id;
+    }
+
+    public function votes()
+    {
+        return $this->morphToMany(User::class, 'votable');
     }
 }
