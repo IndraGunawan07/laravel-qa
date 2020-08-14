80592df8df64d36d7e2ea56b29bad43711442d9b
IndraGunawan07 <53326638+IndraGunawan07@users.noreply.github.com>
2020-08-15 01:04:56 +0700

    test

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <p>test</p>
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
